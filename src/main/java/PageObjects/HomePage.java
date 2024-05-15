7fc6c6da7fe711d1fdb2cad712ba34cfe8fe60ce
shivam rai <shivam.rai@deltatre.com>
2024-05-15 10:55:28 +0530

    updated the test case

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePage {
     WebDriver driver;
 
-    public HomePage(WebDriver driver) {
-        this.driver = driver;
+    public HomePage(WebDriver loadedDriver) {
+        this.driver = loadedDriver;
         PageFactory.initElements(driver, this);
     }
 
--- src/test/java/sampleTest.java
+++ src/test/java/sampleTest.java
@@ -1,14 +1,22 @@
 import PageObjects.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class sampleTest {
+
+    WebDriver driver;
+    HomePage homePage;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shivam.rai\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        homePage = new HomePage(driver);
+    }
     @Test
     public void test1() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shivam.rai\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        HomePage homePage = new HomePage(driver);
         driver.get("https://demoqa.com/elements");
         homePage.selectTextBox();
     }
